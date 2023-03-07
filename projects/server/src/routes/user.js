3c06218c58fa8bbe511200a6752d64448e8be845
randytjioe <randytjioe95@gmail.com>
2023-03-07 17:14:30 +0700

    update redux

--- projects/server/src/routes/user.js
+++ projects/server/src/routes/user.js
@@ -1,6 +1,3 @@
-import express from "express"
-const router = express.Router()
-
-
-
-module.exports = router
+const express = require("express");
+const router = express.Router();
+module.exports = router;
